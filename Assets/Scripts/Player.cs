0407c7ae6e078a90b3c8b1a31c03982b53f6d8fe
Harry Nelken <harry.nelken@gmail.com>
2016-10-29 17:12:52 -0400

    wall slide only active while moving towards wall, plus a little cleanup/commenting
    Signed-off-by: Harry Nelken <harry.nelken@gmail.com>

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,11 +4,11 @@ using System.Collections;
 [RequireComponent (typeof (Controller2D))]
 public class Player : MonoBehaviour {
 
+	// MARK: - Editor properties
 	public float maxJumpHeight = 4;				// Maximum height of a jump
 	public float minJumpHeight = 1; 			// Minimum height of a jump
 	public float jumpTime = .4f;				// Duration of a jump
 	public float moveSpeed = 6;					// Movement speed
-
 	public float wallSlideSpeedMax = 3;			// Maximum descent speed while wall sliding
 	public float wallStickTime = .25f;			// Total that wall stick resists movement away from wall
 	
@@ -16,21 +16,26 @@ public class Player : MonoBehaviour {
 	public Vector2 wallJumpDown;				// Wall jump down velocity vector
 	public Vector2 wallJumpAway;				// Wall jump away velocity vector
 
-	float accTimeAir = .2f;						// Acceleration time while airborne
-	float accTimeGround = .1f;					// Acceleration time while grounded
-	float timeToWallUnstick;					// Remaining time that wall stick resists movement away from wall
 
-	float gravity;								// Strength of the gravity
-	float maxJumpVelocity;						// Maximum velocity of a jump
-	float minJumpVelocity;						// Minimum velocity of a jump
-	float xSmoothing;							// Smoothing factor in x movement
-	Vector3 velocity;							// General velocity vector
+	// MARK: - Private variables
+	private float accTimeAir = .2f;				// Acceleration time while airborne
+	private float accTimeGround = .1f;			// Acceleration time while grounded
+	private float timeToWallUnstick;			// Remaining time that wall stick resists movement away from wall
+	private float gravity;						// Strength of the gravity
+	private float maxJumpVelocity;				// Maximum velocity of a jump
+	private float minJumpVelocity;				// Minimum velocity of a jump
+	private float xSmoothing;					// Smoothing factor in x movement
 
-	Controller2D controller;					// Controller reference
-	
-	Vector2 directionalInput;					// Input vector
-	bool wallSliding;							// True if wall sliding, false otherwise
-	int wallDirX;								// Direction of the wall collision
+	private bool unstuck;						// True if wall sliding unstick time has elapsed and between wall slides
+	private bool wallSliding;					// True if wall sliding
+	private int wallDirX;						// Direction of the wall collision
+
+	private Vector2 velocity;					// 2D velocity vector
+	private Vector2 directionalInput;			// Input vector
+
+	private Controller2D controller;			// Controller reference
+
+
 
 	#region Unity Lifecycle
 	// Initialization
@@ -40,6 +45,7 @@ public class Player : MonoBehaviour {
 		gravity = -(2 * maxJumpHeight) / Mathf.Pow (jumpTime, 2);
 		maxJumpVelocity = Mathf.Abs (gravity) * jumpTime;
 		minJumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(gravity) * minJumpHeight);
+		unstuck = true;
 	}
 
 	// Update is called once per frame
@@ -66,7 +72,7 @@ public class Player : MonoBehaviour {
 	#endregion
 
 	#region Public API
-	// Sets player directional input
+	// Sets player directional input vector
 	public void SetDirectionalInput(Vector2 input) {
 		directionalInput = input;
 	}
@@ -90,17 +96,22 @@ public class Player : MonoBehaviour {
 				velocity.x = -wallDirX * wallJumpAway.x;
 				velocity.y = wallJumpAway.y;
 			}
+			unstuck = true;
 		}
 
-		// Normal jump
+		// Grounded jump
 		if (controller.collisions.below) {
+			// Check if player is sliding down a max slope
 			if (controller.collisions.slidingDownSlope) {
-				if (directionalInput.x != -Mathf.Sign (controller.collisions.slopeNormal.x)) {
+				// Only allow jumps away from slope
+				if (directionalInput.x == Mathf.Sign (controller.collisions.slopeNormal.x)) {
+					// Jump along slope normal
 					velocity.y = maxJumpVelocity * controller.collisions.slopeNormal.y;
 					velocity.x = maxJumpVelocity * controller.collisions.slopeNormal.x;
 				}
 			}
 			else {
+				// Normal jump
 				velocity.y = maxJumpVelocity;
 			}
 		}
@@ -108,6 +119,7 @@ public class Player : MonoBehaviour {
 
 	// Handle variable jump height on jump input up
 	public void OnJumpInputUp() {
+		// Shorten jump height on early release
 		if (velocity.y > minJumpVelocity) {
 			velocity.y = minJumpVelocity;
 		}
@@ -126,6 +138,7 @@ public class Player : MonoBehaviour {
 	private void CheckForWallSliding() {
 		wallSliding = false;
 		wallDirX = (controller.collisions.left) ? -1 : 1;
+
 		if (PlayerIsWallSliding()) {
 			HandleWallSliding();
 		}
@@ -134,27 +147,32 @@ public class Player : MonoBehaviour {
 	// Modify player velocity to handle wall sliding
 	private void HandleWallSliding() {
 		wallSliding = true;
-
-		// Constrain vertical speed to wall slide speed
-		if (velocity.y < -wallSlideSpeedMax) {
-			velocity.y = -wallSlideSpeedMax;
-		}
-
-		// Check or reset wall stick status
-		if (timeToWallUnstick > 0) {
-			xSmoothing = 0;
-			velocity.x = 0;
-
-			// Adjust stick time if player is unsticking
-			if (PlayerIsUnstickingFromWall()) {
-				timeToWallUnstick -= Time.deltaTime;
+		if (directionalInput.x == wallDirX || !unstuck) {
+			if (velocity.y < -wallSlideSpeedMax) {
+				velocity.y = -wallSlideSpeedMax;
 			}
-			else {
+			
+			// Check or reset wall stick status
+			if (timeToWallUnstick > 0) {
+				xSmoothing = 0;
+				velocity.x = 0;
+				
+				// Adjust stick time if player is unsticking
+				if (PlayerIsUnstickingFromWall()) {
+					timeToWallUnstick -= Time.deltaTime;
+					if (timeToWallUnstick <= 0) {
+						unstuck = true;
+					}
+				}
+				else {	// Reset stick time
+					timeToWallUnstick = wallStickTime;
+					unstuck = false;
+				}
+			}
+			else {	// First wall slide frame
 				timeToWallUnstick = wallStickTime;
+				unstuck = false;
 			}
-		}
-		else {
-			timeToWallUnstick = wallStickTime;
 		}
 	}
 
@@ -166,7 +184,7 @@ public class Player : MonoBehaviour {
 
 	// Returns true if the player is moving away from a wall
 	private bool PlayerIsUnstickingFromWall() {
-		return directionalInput.x != wallDirX && directionalInput.x != 0;
+		return directionalInput.x != wallDirX; //&& directionalInput.x != 0;
 	}
 	#endregion
 }
--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -4,7 +4,7 @@ using System.Collections;
 [RequireComponent (typeof (Player))]
 public class PlayerInput : MonoBehaviour {
 
-	private Player player;
+	private Player player;					// The player script to serve input to
 
 	// Use this for initialization
 	void Start () {
@@ -13,9 +13,11 @@ public class PlayerInput : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		// Get raw input vector and pass it to player script
 		Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
 		player.SetDirectionalInput(input);
 
+		// Respond to jump input events
 		if (Input.GetKeyDown(KeyCode.Space)) {
 			player.OnJumpInputDown();
 		}
